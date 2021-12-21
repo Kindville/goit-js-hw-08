3a9faa35845e3ce9211e22bcc7ebcb3159730314
Kindville <87278511+Kindville@users.noreply.github.com>
2021-12-21 21:30:32 +0000

    Deploying to gh-pages from @ Kindville/goit-js-hw-08@ed4876c1586ee5c5cb2f37306170dcb6d4dea132 🚀

--- 02-video.99c3fe3f.js
+++ 02-video.99c3fe3f.js
@@ -7,4 +7,4 @@ var t=arguments[3],n="Expected a function",e=NaN,i="[object Symbol]",r=/^\s+|\s+
 },{}],"iiAF":[function(require,module,exports) {
 "use strict";var e=r(require("@vimeo/player")),t=r(require("lodash.throttle"));function r(e){return e&&e.__esModule?e:{default:e}}const o=document.querySelector("#vimeo-player"),n=new e.default(o),u="videoplayer-current-time",l=localStorage.getItem(u);function a(){n.getCurrentTime().then(function(e){localStorage.setItem(u,e)}).catch(function(e){console.error(e)})}l&&n.setCurrentTime(l),n.on("timeupdate",(0,t.default)(a,1e3));
 },{"@vimeo/player":"zU8P","lodash.throttle":"FNyO"}]},{},["iiAF"], null)
-//# sourceMappingURL=/goit-js-hw-08/02-video.2f3f3436.js.map
+//# sourceMappingURL=/goit-js-hw-08/02-video.99c3fe3f.js.map
